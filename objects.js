b0ffe7b80337182f03561805a96518065b75c934
Wannawuth Damrongchai <wdamrongchai@hotmail.com>
2019-01-07 13:20:24 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,33 @@
+
+
+
+var playlist = new Object({keys: '0'});
+
+ function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+ function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+ }
+
+
+
+
+var playlist=new objects({artistName:'songTitle'});
+
+
+function updatePlayList(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle
+  return playlist
+}
+
+
+
+
+ function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+ }
