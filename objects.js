173fd51f2e66f8a274173aa245bff1f57dbc325f
Wannawuth Damrongchai <wdamrongchai@hotmail.com>
2019-01-07 13:20:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,21 +1,6 @@
 
 
 
-var playlist = new Object({keys: '0'});
-
- function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
-}
-
- function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
-  return playlist
- }
-
-
-
-
 var playlist=new objects({artistName:'songTitle'});
 
 
